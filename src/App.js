b4da84180999603ab8924ba90fc1d30dfe4b421d
warrier70 <anshu.warrier@gmail.com>
2022-09-27 17:58:07 +0530

    fix: commented bootstrap for default

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
--- src/components/BillingFilter.js
+++ src/components/BillingFilter.js
@@ -1,5 +1,5 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 import { Button } from "react-bootstrap";
 import React, { Component } from "react";
 import "./../style/Base.css";
